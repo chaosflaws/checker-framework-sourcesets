89733631bf9aded83e37c3fac9280eb6ff8f8f53
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-01-16 23:18:32 +0100

    Initial commit

---
+++ src/abcd/java/de/chaosflaws/Test2.java
@@ -0,0 +1,10 @@
+package de.chaosflaws;
+
+import java.util.List;
+
+public final class Test2 {
+    public static void main(String[] args) {
+        List<?> test = null;
+        test.add(null);
+    }
+}
---
+++ src/main/java/de/chaosflaws/Test.java
@@ -0,0 +1,10 @@
+package de.chaosflaws;
+
+import java.util.List;
+
+public final class Test {
+    public static void main(String[] args) {
+        List<?> test = null;
+        test.add(null);
+    }
+}
